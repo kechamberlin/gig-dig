33ea2b29978418ca54bd333340f525eed66c35c3
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-06 11:30:08 -0800

    Wrote starter code in Home.js page. There is an h1 tag welcoming the user to the app. A single sentence describing what the app does and how to use it. More info likely to be added later.

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to GigDig!</h1>
+      <p>
+        This app uses the Ticketmaster API to fetch data for music, sports,
+        theater, and family-friendly events.
+      </p>
+    </div>
+  );
+}
+
+export default Home;

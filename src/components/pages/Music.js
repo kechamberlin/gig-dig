0b95ac3d6c3913d0da1bdbc02b38b0f019a98b52
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-06 12:24:12 -0800

    Created a useState hook. The variable name is 'state', update function called 'setState'. State is initially set to an emptry array.

--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import ResultList from '../ResultList';
 
 function Music() {
+  const [state, setState] = useState([]);
+
   return (
     <div>
       <h1>Music Page</h1>

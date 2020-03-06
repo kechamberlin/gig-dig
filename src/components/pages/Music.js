8d6a9b14ce1ac03642a27a0f07da8974757a4b96
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-06 12:40:38 -0800

    Created a getData async-await function. The API_KEY is stored as a variable. I use the fetch method to grab the API data from Ticketmaster. The data is console logged. setState to the data.

--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -8,6 +8,17 @@ function Music() {
     getData();
   }, []);
 
+  const getData = async () => {
+    // In a real application, the API_KEY would be an environment variable.
+    const API_KEY = 'xEC8368Dovp9nNnNBhtG5kDPJ3D8NjGC';
+    const response = await fetch(
+      `https://app.ticketmaster.com/discovery/v2/events?classificationName=music&apikey=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data._embedded.events);
+    setState(data._embedded.events);
+  };
+
   return (
     <div>
       <h1>Music Page</h1>

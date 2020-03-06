2d8af24040568a2c959b85e9ac73338317440bdd
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-06 11:14:40 -0800

    Added a comment above icon reminding me to make the logo a Link to the home page. Created an unordered list with 4 list items: - Music - Sports - Theater - Family

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,15 @@ function Navbar() {
   return (
     <div>
       <h1>
+        {/* Home page link */}
         <i className='fas fa-gem'>GigDig</i>
       </h1>
+      <ul>
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Theater</li>
+          <li>Family</li>
+      </ul>
     </div>
   );
 }

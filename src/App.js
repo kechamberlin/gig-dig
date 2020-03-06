b8d3c6830c7265f1f47e7fc8ec701f1694d6ed9d
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-06 11:53:32 -0800

    Imported Music, Sports, Theater, and Family page components.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
+import Music from './components/pages/Music';
+import Sports from './components/pages/Sports';
+import Theater from './components/pages/Theater';
+import Family from './components/pages/Family';
 import './App.css';
 
 function App() {

d88a390045bf0015d76d6851a1df9f91f14cae50
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-06 11:15:59 -0800

    Imported BrowserRouter as Router, Switch, Router from react-router-dom. Added Navbar component to return statement. Minor formatting fixes.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <Navbar />
     </div>
   );
 }

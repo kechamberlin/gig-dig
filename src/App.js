52c4346afc759d753196e218bf3aff6dbef3f528
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-07 12:21:38 -0800

    Imported Search component. Added Search component underneath Navbar component.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Music from './components/pages/Music';
 import Sports from './components/pages/Sports';
 import Theater from './components/pages/Theater';
 import Family from './components/pages/Family';
+import Search from "./components/Search";
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Router>
       <div className='App'>
         <Navbar />
+        <Search />
         <div className="container">
           <Switch>
             <Route exact path='/' component={Home} />

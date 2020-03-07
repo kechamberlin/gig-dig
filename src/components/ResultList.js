9c6fdb981159ac66519f2d1867fc1ed587ea06c3
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-07 12:08:04 -0800

    Gave the div wrapper a className of card.

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ResultList({ name, image }) {
   return (
-    <div>
+    <div className='card'>
       <h4>{name}</h4>
       <img src={image} alt='' />
     </div>

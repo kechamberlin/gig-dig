08af24850cec8576ee58643cde5cb9bb40845e84
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-07 18:27:34 -0800

    Removed reference to family-friendly events in paragraph tag.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,8 +5,8 @@ function Home() {
     <div>
       <h1>Welcome to GigDig!</h1>
       <p>
-        This app uses the Ticketmaster API to fetch data for music, sports,
-        theater, and family-friendly events.
+        This app uses the Ticketmaster API to fetch data for music, sports, and
+        theater events.
       </p>
     </div>
   );

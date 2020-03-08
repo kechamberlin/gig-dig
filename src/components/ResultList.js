3c5e13d8c8f1f6d975449bab1f81fd5e3f59920d
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-07 16:53:39 -0800

    Added date, time, venue, and city parameters and put them in paragraph tags.

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-function ResultList({ name, image }) {
+function ResultList({ name, image, date, time, venue, city }) {
   return (
     <div className='card'>
       <h4>{name}</h4>
       <img src={image} alt='' />
+      <p>Date: {date}</p>
+      <p>When: {time}</p>
+      <p>Where: {venue}</p>
+      <p>
+        {city}
+      </p>
     </div>
   );
 }

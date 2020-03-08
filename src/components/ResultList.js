c50de6522e59e6b36842179fa2797020733b5a0c
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-07 17:02:34 -0800

    Shifted around some of the parameters. Venue was moved to below the image.

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-function ResultList({ name, image, date, time, venue, city }) {
+function ResultList({ name, image, venue, date, time, city }) {
   return (
     <div className='card'>
       <h4>{name}</h4>
       <img src={image} alt='' />
-      <p>Date: {date}</p>
-      <p>When: {time}</p>
-      <p>Where: {venue}</p>
-      <p>
-        {city}
-      </p>
+      <p>{venue}</p>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>{city}</p>
     </div>
   );
 }

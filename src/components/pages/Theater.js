b2f3402dd814734f500426a99d26932819465710
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-07 18:13:22 -0800

    Copy and pasted code from Music page component into Theater page component. Changed classificationName in API link query from music to arts.

--- src/components/pages/Theater.js
+++ src/components/pages/Theater.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Search from '../Search';
+import ResultList from '../ResultList';
+import Loading from '../Loading';
 
 function Theater() {
+  const [state, setState] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    searchShows();
+  }, []);
+
+  const searchShows = async keyword => {
+    setLoading(true);
+    // In a real application, the API_KEY would be an environment variable.
+    const API_KEY = 'xEC8368Dovp9nNnNBhtG5kDPJ3D8NjGC';
+    const response = await fetch(
+      `https://app.ticketmaster.com/discovery/v2/events?classificationName=arts&keyword=${keyword}&apikey=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data._embedded.events);
+    setState(data._embedded.events);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <div>
-      <h1>Theater Page</h1>
+      <Search searchShows={searchShows} />
+      <div className='grid-3'>
+        {state.map(show => (
+          <ResultList
+            key={show.id}
+            name={show.name}
+            image={show.images[9].url}
+            date={show.dates.start.localDate}
+            time={show.dates.start.localTime}
+            venue={show._embedded.venues[0].name}
+            city={show._embedded.venues[0].city.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }

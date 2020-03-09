0b149527e5610c6e5e81e0d98e0db0cd231bf05a
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-09 13:05:36 -0700

    Created a Jumbotron.js component with basic starter code. Imported Jumbotron from react-bootstrap. Added Jumbotron component code from react-bootstrap to the return statement.

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap';
+
+function MyJumbotron() {
+  return (
+    <Jumbotron fluid>
+      <h1>Fluid jumbotron</h1>
+      <p>
+        This is a modified jumbotron that occupies the entire horizontal space
+        of its parent.
+      </p>
+    </Jumbotron>
+  );
+}
+
+export default MyJumbotron;

5cb140978a0143de5778a6e1a48138a37806f50f
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-03-09 10:04:22 -0700

    As with the Music and Sports page components, added error state, wrapped fetch method in a try/catch statement, and made return statement an if/else where if error, return sorry message, else return the mapped query.

--- src/components/pages/Theater.js
+++ src/components/pages/Theater.js
@@ -6,45 +6,63 @@ import Loading from '../Loading';
 function Theater() {
   const [state, setState] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     searchShows();
   }, []);
 
   const searchShows = async keyword => {
+    setError(false);
     setLoading(true);
-    // In a real application, the API_KEY would be an environment variable.
-    const API_KEY = 'xEC8368Dovp9nNnNBhtG5kDPJ3D8NjGC';
-    const response = await fetch(
-      `https://app.ticketmaster.com/discovery/v2/events?classificationName=arts&keyword=${keyword}&apikey=${API_KEY}`
-    );
-    const data = await response.json();
-    console.log(data._embedded.events);
-    setState(data._embedded.events);
+
+    try {
+      // In a real application, the API_KEY would be an environment variable.
+      const API_KEY = 'xEC8368Dovp9nNnNBhtG5kDPJ3D8NjGC';
+      const response = await fetch(
+        `https://app.ticketmaster.com/discovery/v2/events?classificationName=arts&keyword=${keyword}&apikey=${API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data._embedded.events);
+      setState(data._embedded.events);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
     setLoading(false);
   };
 
   if (loading) {
     return <Loading />;
   }
-  return (
-    <div>
-      <Search searchShows={searchShows} />
-      <div className='grid-3'>
-        {state.map(show => (
-          <ResultList
-            key={show.id}
-            name={show.name}
-            image={show.images[9].url}
-            date={show.dates.start.localDate}
-            time={show.dates.start.localTime}
-            venue={show._embedded.venues[0].name}
-            city={show._embedded.venues[0].city.name}
-          />
-        ))}
+
+  if (error) {
+    return (
+      <div>
+        <Search searchShows={searchShows} />
+        <p>Sorry, no match</p>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <Search searchShows={searchShows} />
+        <div className='grid-3'>
+          {state.map(show => (
+            <ResultList
+              key={show.id}
+              name={show.name}
+              image={show.images[9].url}
+              date={show.dates.start.localDate}
+              time={show.dates.start.localTime}
+              venue={show._embedded.venues[0].name}
+              city={show._embedded.venues[0].city.name}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Theater;

614bf08950686bfa965b164435e8b24042216b1f
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-05-20 20:44:39 -0700

    Updates

---
+++ precache-manifest.c8d53c8a67f90d7869b145734686f957.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663df1972844579a3b33a63104e5576f",
+    "url": "/gig-dig/index.html"
+  },
+  {
+    "revision": "cb4946c2a267fd31b999",
+    "url": "/gig-dig/static/css/main.18362e7d.chunk.css"
+  },
+  {
+    "revision": "504fecb9a70305f94990",
+    "url": "/gig-dig/static/js/2.1c9cb339.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/gig-dig/static/js/2.1c9cb339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb4946c2a267fd31b999",
+    "url": "/gig-dig/static/js/main.3acc013e.chunk.js"
+  },
+  {
+    "revision": "eb6dc5146a1fdf2c9cdd",
+    "url": "/gig-dig/static/js/runtime-main.4d8e7b8c.js"
+  },
+  {
+    "revision": "32100015cb517b767f9cbfe99ec9786c",
+    "url": "/gig-dig/static/media/loading.32100015.gif"
+  },
+  {
+    "revision": "22197cc2bb401772239d92af4b778793",
+    "url": "/gig-dig/static/media/music-jumbotron.22197cc2.jpg"
+  },
+  {
+    "revision": "aac0f9d94275991d05d133db700af3ea",
+    "url": "/gig-dig/static/media/sports-jumbotron.aac0f9d9.jpg"
+  },
+  {
+    "revision": "6bc9c0cfb630c34f0c7bf0931812bd01",
+    "url": "/gig-dig/static/media/theater-jumbotron.6bc9c0cf.jpg"
+  }
+]);

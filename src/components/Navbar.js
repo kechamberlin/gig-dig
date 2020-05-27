38536212df3665a5f5b557569f2f5f868e4bce35
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-05-27 10:18:30 -0700

    Altered Link paths.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,23 +5,23 @@ function Navbar() {
   return (
     <nav className='navbar bg-primary'>
       <h1>
-        <Link to='/'>
+        <Link to='https://kechamberlin.github.io/gig-dig/'>
           <i className='fas fa-check-double'>GigDig</i>
         </Link>
       </h1>
       <ul>
         <li>
-          <Link to='/music'>
+          <Link to='https://kechamberlin.github.io/gig-dig/music'>
             <i className='fas fa-music'></i> Music
           </Link>
         </li>
         <li>
-          <Link to='/sports'>
+          <Link to='https://kechamberlin.github.io/gig-dig/sports'>
             <i className='fas fa-football-ball'></i> Sports
           </Link>
         </li>
         <li>
-          <Link to='/theater'>
+          <Link to='https://kechamberlin.github.io/gig-dig/theater'>
             <i className='fas fa-theater-masks'></i> Theater
           </Link>
         </li>
